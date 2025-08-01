381a2d5f1172ca04e443dbf538ed820065c2578e
Gideon Sebastian Geier <gideon.sebastian.geier@huawei.com>
2025-08-01 21:16:55 +0800

    add failing mmap test
    Signed-off-by: Gideon Sebastian Geier <gideon.sebastian.geier@huawei.com>

---
+++ test/simple_mmap_test.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+int
+main(void)
+{
+    void *ptr1 = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+                                  MAP_ANONYMOUS ,0, 0);
+
+    munmap(ptr1, 1);
+    return 0;
+}
