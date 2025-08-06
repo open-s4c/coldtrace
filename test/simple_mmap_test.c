323653e43fce77b8843d3fc862c24355be1a95bf
Gigna <georgeta.igna@huawei.com>
2025-08-06 13:03:25 +0200

    CI pipeline - first tests
    Signed-off-by: Gigna <georgeta.igna@huawei.com>

---
+++ test/simple_mmap_test.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+int
+main(void)
+{
+    void *ptr1 = mmap(NULL, 257, PROT_READ | PROT_WRITE,
+                                  MAP_PRIVATE | MAP_ANONYMOUS ,0, 0);
+
+    munmap(ptr1, 1);
+    return 0;
+}
---
+++ test/simple_mutex_test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t lock;
+
+void* increment(void* arg) {
+    for (int i = 0; i < 100000; i++) {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    // Initialize the mutex
+    pthread_mutex_init(&lock, NULL);
+
+    // Create two threads
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+
+    // Wait for both threads to finish
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    // Destroy the mutex
+    pthread_mutex_destroy(&lock);
+
+    printf("Final counter value: %d\n", counter);
+    return 0;
+}
