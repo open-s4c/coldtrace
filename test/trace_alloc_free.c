00e31b71e2da7a17d0c94d40d7bd2be62ffab64d
Anastasia Basho <anastasia.basho@huawei.com>
2025-09-23 18:48:19 +0300

    Test for free and mmap (#36)
    * Add trace alloc-free test
    * Add mmap and munmap test
    
    Signed-off-by: Anastasia Basho <anastasia.basho@huawei.com>

---
+++ test/trace_alloc_free.c
@@ -0,0 +1,33 @@
+#include <dice/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <trace_checker.h>
+
+
+struct entry_expect expected_1[] = {
+    EXPECT_ENTRY(COLDTRACE_ALLOC),
+    EXPECT_ENTRY(COLDTRACE_ALLOC),
+    EXPECT_ENTRY(COLDTRACE_WRITE),
+    EXPECT_ENTRY(COLDTRACE_FREE),
+    EXPECT_ENTRY(COLDTRACE_THREAD_EXIT),
+    EXPECT_END,
+};
+
+int
+main()
+{
+    register_expected_trace(1, expected_1);
+    int *ptr1;
+    ptr1 = malloc(sizeof(*ptr1));
+
+    if (ptr1 == NULL) {
+        log_fatal("Null pointer has been returned");
+    }
+
+    *ptr1 = 20;
+
+    free(ptr1);
+    ptr1 = NULL;
+
+    return 0;
+}
