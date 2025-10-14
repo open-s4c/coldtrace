10cfd404ffa75c4c4d6ffae56db16eb201ddf1e3
Anastasia Basho <anastasia.basho@huawei.com>
2025-10-14 12:01:08 +0200

    trace checker fix (#45)
    fix expected trace modification in trace_checker
    remove code duplicate in trace_atomic
    
    Signed-off-by: Anastasia Basho <anastasia.basho@huawei.com>
    Signed-off-by: Gideon Sebastian Geier <gideon.sebastian.geier@huawei.com>

--- test/trace_atomic.cpp
+++ test/trace_atomic.cpp
@@ -1,5 +1,4 @@
 #include <atomic>
-#include <cinttypes>
 #include <cstdio>
 #include <cstdlib>
 #include <pthread.h>
@@ -11,11 +10,6 @@
 #define ATOMIC_RW_CYCLE                                                        \
     EXPECT_ENTRY(COLDTRACE_ATOMIC_READ), EXPECT_ENTRY(COLDTRACE_ATOMIC_WRITE), \
         EXPECT_SOME(COLDTRACE_READ, 0, 1), EXPECT_SOME(COLDTRACE_WRITE, 0, 1)
-
-#define ATOMIC_RW_CYCLE_3                                                      \
-    EXPECT_ENTRY(COLDTRACE_ATOMIC_READ), EXPECT_ENTRY(COLDTRACE_ATOMIC_WRITE), \
-        EXPECT_SOME(COLDTRACE_READ, 0, 1), EXPECT_SOME(COLDTRACE_WRITE, 0, 1)
-
 
 struct expected_entry expected_1[] = {
     EXPECT_ENTRY(COLDTRACE_ALLOC),
@@ -52,24 +46,8 @@ struct expected_entry expected_2[] = {
 
 struct expected_entry expected_3[] = {
     EXPECT_ENTRY(COLDTRACE_THREAD_START),
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    ATOMIC_RW_CYCLE_3,
-    EXPECT_ENTRY(COLDTRACE_THREAD_EXIT),
-    EXPECT_END,
-};
-
-struct expected_entry expected_4[] = {
-    EXPECT_ENTRY(COLDTRACE_THREAD_START),
     EXPECT_ENTRY(COLDTRACE_ATOMIC_READ),
-    EXPECT_SOME(COLDTRACE_ATOMIC_READ, 0, 100),
+    EXPECT_SOME(COLDTRACE_ATOMIC_READ, 0, 1000),
     EXPECT_SOME(COLDTRACE_READ, 0, 1),
     EXPECT_SOME(COLDTRACE_ALLOC, 0, 1),
     EXPECT_SOME(COLDTRACE_FREE, 0, 1),
@@ -110,8 +88,8 @@ main()
 {
     register_expected_trace(1, expected_1);
     register_expected_trace(2, expected_2);
-    register_expected_trace(3, expected_3);
-    register_expected_trace(4, expected_4);
+    register_expected_trace(3, expected_2);
+    register_expected_trace(4, expected_3);
 
     pthread_t threads[NUM_THREADS];
     pthread_t free_thread;
